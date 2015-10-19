13fa6d3095ca6e8a669b943d1e1482a75ee0734b
Paolo Pialorsi <paolo@pialorsi.com>
2015-10-19 17:26:30 +0200

    Removed jQuery UI and removed template selection from SiteInformation step.

--- OfficeDevPnP.PartnerPack.SiteProvisioning/OfficeDevPnP.PartnerPack.SiteProvisioning/App_Start/BundleConfig.cs
+++ OfficeDevPnP.PartnerPack.SiteProvisioning/OfficeDevPnP.PartnerPack.SiteProvisioning/App_Start/BundleConfig.cs
@@ -30,31 +30,6 @@ namespace OfficeDevPnP.PartnerPack.SiteProvisioning
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                    "~/Scripts/jquery-ui-{version}.js"));
-
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                    "~/Content/themes/base/accordion.css",
-                    "~/Content/themes/base/all.css",
-                    "~/Content/themes/base/autocomplete.css",
-                    "~/Content/themes/base/base.css",
-                    "~/Content/themes/base/button.css",
-                    "~/Content/themes/base/core.css",
-                    "~/Content/themes/base/datepicker.css",
-                    "~/Content/themes/base/dialog.css",
-                    "~/Content/themes/base/draggable.css",
-                    "~/Content/themes/base/menu.css",
-                    "~/Content/themes/base/progressbar.css",
-                    "~/Content/themes/base/resizable.css",
-                    "~/Content/themes/base/selectable.css",
-                    "~/Content/themes/base/selectmenu.css",
-                    "~/Content/themes/base/slider.css",
-                    "~/Content/themes/base/sortable.css",
-                    "~/Content/themes/base/spinner.css",
-                    "~/Content/themes/base/tabs.css",
-                    "~/Content/themes/base/theme.css",
-                    "~/Content/themes/base/tooltip.css"));
         }
     }
 }
