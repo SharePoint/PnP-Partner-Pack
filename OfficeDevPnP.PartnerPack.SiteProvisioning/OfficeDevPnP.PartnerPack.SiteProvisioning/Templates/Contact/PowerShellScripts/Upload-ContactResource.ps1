f5c3a0ee88975d1d3a41912568887137095dd5c7
Sriram Sundaresan <sriramguhan2010@outlook.com>
2015-12-22 15:26:04 +0530

    Created similar FAB40 template for Contacts and Helpdesk

---
+++ OfficeDevPnP.PartnerPack.SiteProvisioning/OfficeDevPnP.PartnerPack.SiteProvisioning/Templates/Contact/PowerShellScripts/Upload-ContactResource.ps1
@@ -0,0 +1,33 @@
+﻿<#
+.SYNOPSIS
+Uploads the template in the PnP PartnerPack Infra Site
+
+.EXAMPLE
+PS C:\> .\Contact.ps1 `
+-PnpInfrasite "https://mytenant.sharepoint.com/sites/infrastructure" `
+-Csspath  "c:\Templates\Contact.css" `
+-Jspath "c:\Templates\Contact.min.js" `
+#>
+
+
+
+
+param
+(
+     [Parameter(Mandatory = $true, HelpMessage="Enter the Url of PnP Infrastructural site")]
+    [String]
+    $PnpInfrasite,
+
+    [Parameter(Mandatory = $true, HelpMessage="Enter the path of contact.css")]
+    [String]
+    $Csspath,
+
+    [Parameter(Mandatory = $true, HelpMessage="Enter the path of contact.min.js")]
+    [String]
+    $Jspath
+
+)
+
+Connect-SPOnline $PnpInfrasite
+Add-SPOFile -Path $Csspath -Folder "PnPProvisioningTemplates"
+Add-SPOFile -Path $Jspath -Folder "PnPProvisioningTemplates"
