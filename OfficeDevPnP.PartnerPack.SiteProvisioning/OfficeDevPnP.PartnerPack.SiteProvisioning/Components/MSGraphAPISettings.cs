422100178ba84adfaf5227116f12c3e3e200db38
Paolo Pialorsi <paolo@pialorsi.com>
2016-02-05 18:56:48 +0100

    Fixed site template selection validation.

--- OfficeDevPnP.PartnerPack.SiteProvisioning/OfficeDevPnP.PartnerPack.SiteProvisioning/Components/MSGraphAPISettings.cs
+++ OfficeDevPnP.PartnerPack.SiteProvisioning/OfficeDevPnP.PartnerPack.SiteProvisioning/Components/MSGraphAPISettings.cs
@@ -12,7 +12,6 @@ namespace OfficeDevPnP.PartnerPack.SiteProvisioning.Components
         public static string ClientSecret = ConfigurationManager.AppSettings["ida:ClientSecret"];
         public static string AADInstance = ConfigurationManager.AppSettings["ida:AADInstance"];
         public static string TenantId = ConfigurationManager.AppSettings["ida:TenantId"];
-        public static string PostLogoutRedirectUri = ConfigurationManager.AppSettings["ida:PostLogoutRedirectUri"];
         public static string Authority = AADInstance + TenantId;
         public static string MicrosoftGraphResourceId = "https://graph.microsoft.com";
     }
