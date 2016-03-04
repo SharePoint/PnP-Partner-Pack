66da17a09a64afac9ae5a97ceef4a87a52d26d66
Bert Jansen <bjansen@microsoft.com>
2016-03-04 09:43:10 +0100

    added script to grab certificate information to be pasted in the Azure AD json app manifest

---
+++ scripts/Get-SelfSignedCertificateInformation.ps1
@@ -0,0 +1,22 @@
+﻿$certPath = Read-Host "Enter certificate path (.cer)"
+$cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2
+$cert.Import($certPath)
+$rawCert = $cert.GetRawCertData()
+$base64Cert = [System.Convert]::ToBase64String($rawCert)
+$rawCertHash = $cert.GetCertHash()
+$base64CertHash = [System.Convert]::ToBase64String($rawCertHash)
+$KeyId = [System.Guid]::NewGuid().ToString()
+
+$keyCredentials = 
+'"keyCredentials": [
+    {
+      "customKeyIdentifier": "'+ $base64CertHash + '",
+      "keyId": "' + $KeyId + '",
+      "type": "AsymmetricX509Cert",
+      "usage": "Verify",
+      "value":  "' + $base64Cert + '"
+     }
+  ],'
+$keyCredentials
+
+Write-Host "Certificate Thumbprint:" $cert.Thumbprint
