c017dbd6cdcccd0a3aaa5b93bf094015199fb466
Lee Dale <lee.jdale@gmail.com>
2017-01-30 09:34:09 +0000

    Fixed spelling mistake in logging. "Applyed" to "Applied"

--- OfficeDevPnP.PartnerPack.SiteProvisioning/OfficeDevPnP.PartnerPack.Infrastructure/Jobs/Handlers/SiteCollectionProvisioningJobHandler.cs
+++ OfficeDevPnP.PartnerPack.SiteProvisioning/OfficeDevPnP.PartnerPack.Infrastructure/Jobs/Handlers/SiteCollectionProvisioningJobHandler.cs
@@ -231,7 +231,7 @@ namespace OfficeDevPnP.PartnerPack.Infrastructure.Jobs.Handlers
                         }
 
 
-                        Console.WriteLine("Applyed Provisioning Template \"{0}\" to site.",
+                        Console.WriteLine("Applied Provisioning Template \"{0}\" to site.",
                             job.ProvisioningTemplateUrl);
                     }
                 }
