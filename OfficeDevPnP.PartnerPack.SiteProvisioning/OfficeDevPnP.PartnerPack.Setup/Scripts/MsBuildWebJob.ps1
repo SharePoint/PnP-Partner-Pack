a79402a0ea6f3ef614f133ddc35af965ef4c576b
paolo@pialorsi.com <paolo@pialorsi.com>
2017-05-17 18:22:41 +0200

    Fix toprovide better support for VS2017

--- OfficeDevPnP.PartnerPack.SiteProvisioning/OfficeDevPnP.PartnerPack.Setup/Scripts/MsBuildWebJob.ps1
+++ OfficeDevPnP.PartnerPack.SiteProvisioning/OfficeDevPnP.PartnerPack.Setup/Scripts/MsBuildWebJob.ps1
@@ -5,13 +5,26 @@
 
 pushd $ProjectPath
 
-# Get the path of MSBuild v. 14.0
-$msbuildPath = Get-ItemProperty "hklm:\SOFTWARE\Microsoft\MSBuild\14.0"
-$msbuildPath = $msbuildPath.MSBuildOverrideTasksPath.Substring(0, $msbuildPath.MSBuildOverrideTasksPath.IndexOf("\bin\") + 5)
-$msbuildPath = $msbuildPath + "MSBuild.exe"
+# Get the path of MSBuild v. 14.0.25420.1 or higher
+$vs15 = Get-ItemProperty "hklm:\SOFTWARE\WOW6432Node\Microsoft\VisualStudio\SxS\VS7"
+$msBuild15Path = $vs15.'15.0' + "\MSBuild\15.0\bin\msbuild.exe"
+$vsVersion = "15.0"
+if (Test-Path $msBuild15Path)
+{
+    $msbuildPath = $msBuild15Path
+}
 
-& $msbuildPath /p:Configuration=Release /p:VisualStudioVersion="14.0" 
+if ($msbuildPath -eq $null)
+{
+    $msbuildPath = Get-ItemProperty "hklm:\SOFTWARE\Microsoft\MSBuild\14.0"
+    $msbuildPath = $msbuildPath.MSBuildOverrideTasksPath.Substring(0, $msbuildPath.MSBuildOverrideTasksPath.IndexOf("\bin\") + 5)
+    $msbuildPath = $msbuildPath + "MSBuild.exe"
+	$vsVersion = "14.0"
+}
+
+& $msbuildPath /p:Configuration=Release /p:VisualStudioVersion="$vsVersion" 
 # /p:PublishSettingsFile="$PublishingSettingsPath" /p:DeployOnBuild=true 
 # /p:AllowUntrustedCertificate=true /p:_DestinationType=AzureWebSite
 
 popd
+
--- OfficeDevPnP.PartnerPack.SiteProvisioning/OfficeDevPnP.PartnerPack.Setup/Scripts/MsBuildWebSite.ps1
+++ OfficeDevPnP.PartnerPack.SiteProvisioning/OfficeDevPnP.PartnerPack.Setup/Scripts/MsBuildWebSite.ps1
@@ -8,6 +8,7 @@ pushd $ProjectPath
 # Get the path of MSBuild v. 14.0.25420.1 or higher
 $vs15 = Get-ItemProperty "hklm:\SOFTWARE\WOW6432Node\Microsoft\VisualStudio\SxS\VS7"
 $msBuild15Path = $vs15.'15.0' + "\MSBuild\15.0\bin\msbuild.exe"
+$vsVersion = "15.0"
 if (Test-Path $msBuild15Path)
 {
     $msbuildPath = $msBuild15Path
@@ -18,6 +19,7 @@ if ($msbuildPath -eq $null)
     $msbuildPath = Get-ItemProperty "hklm:\SOFTWARE\Microsoft\MSBuild\14.0"
     $msbuildPath = $msbuildPath.MSBuildOverrideTasksPath.Substring(0, $msbuildPath.MSBuildOverrideTasksPath.IndexOf("\bin\") + 5)
     $msbuildPath = $msbuildPath + "MSBuild.exe"
+	$vsVersion = "14.0"
 }
 
 if ((Test-Path $msBuild15Path) -ne $true)
@@ -31,8 +33,9 @@ $tempPath = "$env:SystemDrive\PnP$unique"
 mkdir $tempPath
 
 # Run MSBuild to build and deploy the solution
-& "$msbuildPath" /p:Configuration=Release /p:OutputPath="$tempPath" /p:VisualStudioVersion="14.0" /p:PublishSettingsFile="$PublishingSettingsPath" /p:DeployOnBuild=true
+& "$msbuildPath" /p:Configuration=Release /p:OutputPath="$tempPath" /p:VisualStudioVersion="$vsVersion" /p:PublishSettingsFile="$PublishingSettingsPath" /p:DeployOnBuild=true
 
 Remove-Item $tempPath -Recurse -Force
 popd
+
 
